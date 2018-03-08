661fd0ea946d74765a653ad107ab6c2c29360d37
Au Yong Min Hao <mauyong@moneylion.com>
2018-03-09 01:05:57 +0800

    Readd the full automation

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import os
+import re
 
 import requests
 import discord
 import asyncio
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-DISCORD_WEBHOOK = os.getenv("DISCORD_RAINDANCE_WEBHOOK")
+# DISCORD_WEBHOOK = os.getenv("DISCORD_RAINDANCE_WEBHOOK")
 SLACK_WEBHOOK = os.getenv("SLACK_RAINDANCE_WEBHOOK")
-SERVER_LIST = ['turtlecoin']
+TURTLE_ADDRESS = os.getenv("TURTLE_ADDRESS", "")
+TURTLECOIN_SERVER_ID = '388915017187328002'
+RAINDANCE_CHANNEL_ID = '407757902695235584'
+TURTLERAINBOT_ID = '407753551859810304'
 
 client = discord.Client()
 
-def is_targeted_message(message):
-    return any(server in message.server.name.lower() for server in SERVER_LIST)
-
-def notify(text):
+async def notify(text):
     print(text)
-    if DISCORD_WEBHOOK:
-        requests.post(DISCORD_WEBHOOK, json={"content": text})
+    # if DISCORD_WEBHOOK:
+    #     print("Notifying Discord..")
+    #     requests.post(DISCORD_WEBHOOK, json={"content": text})
     
     if SLACK_WEBHOOK:
+        print("Notifying Slack..")
         requests.post(SLACK_WEBHOOK, json={"text": text})
 
+def extract_title(message):
+    return message.embeds[0].get('title', '').lower()
+
 def extract_description(message):
-    return message.embeds[0].get('description', '')
+    return message.embeds[0].get('description', '').lower()
 
 def message_contains(message, condition):
     conditions = {
@@ -32,7 +38,13 @@ def message_contains(message, condition):
         'send_address': "send me your wallet address",
         'react': ''
     }
-    return conditions.get(condition, '') in extract_description(message)
+    return conditions.get(condition, 'N/A') in extract_title(message)
+
+def print_message(message):
+    print("TurtleMessage:", message.content)
+    print("Author:", message.author)
+    print("Embeds:", message.embeds)
+    print("ID:", message.id)
 
 @client.event
 async def on_ready():
@@ -43,49 +55,55 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    # Ignore PMs and messages on other servers
-    if message.server is None:
-        print("PM:", message)
+    if (message.channel.id != RAINDANCE_CHANNEL_ID) or (message.author.id != TURTLERAINBOT_ID):
         return
-    elif not is_targeted_message(message):
+    elif not message.embeds: # Check for attachments
         return
-
-    # Check for rain
-    if 'turtlecoin' in message.server.name.lower():
-        if message.embeds:
-            if message_contains(message, 'tut_tut'):
-                notify("It's gonna rain!")
-
-                # # Wait for it to rain
-                # await asyncio.sleep(10)
-                # async for log in client.logs_from(message.channel, author=message.author, limit=3, reverse=True):
-                #     if message_contains(message, 'raining'):
-                #         notify("It's raining!")
-
-                # # Wait for the cue to send address
-                # await asyncio.sleep(10)
-                # async for log in client.logs_from(message.channel, author=message.author, limit=3, reverse=True):
-                #     if message_contains(message, 'send_address'):
-                #         notify("Alright, time to send your address!")
-
-                # # Wait for reaction
-
-                # notify("Reacting to the rain lol.")
     
-    # elif message.content.startswith('$'):
-    #     print("Message:", message.content)
-    #     # await client.send_message(message.channel, "Yep got it")
-    #     channel = client.get_channel('407757902695235584')
-    #     async for tm in client.logs_from(channel, limit=2, reverse=True):
-    #         print("TurtleMessage:", tm.content)
-    #         print("Author:", tm.author)
-    #         print("Attachments:", tm.attachments)
-    #         print("ID:", tm.id)
-    #         print("Timestamp:", tm.timestamp)
-    #         print("Edited:", tm.edited_timestamp)
-    #         print("Type:", tm.type)
-    #         print("SystemContent:", tm.system_content)
-    #         print("Embeds:", tm.embeds)
+    emoji_list = message.server.emojis
+
+    print_message(message)
+    print("Emojis:", emoji_list)
+
+    # Check for rain    
+    if not message_contains(message, 'tut_tut'):
+        return
     
+    # OK, it's gonna rain
+    await notify("It's gonna rain!")
+
+    # Wait for it to rain
+    raining = False
+    while not raining:
+        await asyncio.sleep(5)
+        async for log in client.logs_from(message.channel, limit=2, reverse=True):
+            if message_contains(log, 'raining') and (log.author == message.author):
+                raining = True
+                await notify("It's raining!")
+                print_message(log)
+
+    # Wait for the cue to send address
+    address_requested = False
+    while not address_requested:
+        await asyncio.sleep(10)
+        async for log in client.logs_from(message.channel, limit=2, reverse=True):
+            if message_contains(log, 'send_address') and (log.author == message.author):
+                address_requested = True
+                await notify("Sending your address!")
+                await client.send_message(message.author, TURTLE_ADDRESS)
+                print_message(log)
+
+    # Wait for reaction
+    private_message = await client.wait_for_message(timeout=15, author=message.author)
+    print_message(private_message)
+    regex = re.search(r"<\S{22,}>", message.content)
+    if regex is not None:
+        for emoji in elist:
+            if str(emoji) == regex.group(0):
+                await notify("Reacting to the rain lol.")
+                await bot.add_reaction(rain_message, emoji)
+    else:
+        notify("Emoji not found, GG.")
+
 
 client.run(TOKEN, bot=False)
